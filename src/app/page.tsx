a4d077da538fed5403182b141f909673ef397263
bhanusunrise <pasindubhanukagood@gmail.com>
2024-11-19 14:01:13 +0530

    div added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,9 @@ import NavBar from "../components/navbar/navbar"
 
 export default function Page() {
   return<> 
+  <div className="container mx-auto px-4">
     <NavBar/>
     <h1>Hello, Next.js!</h1>
+  </div>
   </>
 }
